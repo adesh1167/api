e0443938c0333a86ab722fbd175fee6ffe0c48f7
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2024-09-28 11:27:16 +0100

    Update and rename public/send-expo-notification.php to public/aviator/check-access-code.php

---
+++ public/aviator/check-access-code.php
@@ -0,0 +1,34 @@
+<?php
+// Set CORS headers to allow requests from any origin (for development only, restrict in production)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight requests for CORS
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
+// Read JSON input from request body
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+
+$response = [];
+$access_code = $_GET['access-code'];
+
+$access_codes = [
+    'sporty0924880au2',
+    'sporty09248o301c',
+];
+
+if(isset($access_code) && !empty($access_code)){
+    if(in_array($access_codes, $access_code)){
+        $response['status'] = 'valid';
+        $response['message'] = 'Access Code Valid. Server overload, try again';
+    } else{
+        $response['status'] = 'invalid';
+        $response['error'] = 'Access Code Invalid';
+    }
+}
+
+exit(json_encode($response));
