e53631df7e225c0aa7750fc510c1761e42b5f484
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2024-10-08 13:19:53 +0100

    Create showinghero.php

---
+++ public/watermark/showinghero.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_GET["type"])){
+	if($_GET["type"] == "image") header("Content-Type: image/jpeg");
+    echo file_get_contents($_GET['url']);
+} else {
+    $url = $_GET['url'];
+    $options = [
+      
+        "http" => [
+            "header" => "User-Agent: Mozilla/5.0\r\n"
+        ],
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ]
+    ];
+
+    $context = stream_context_create($options);
+    $data = @file_get_contents("https://showinghero.com/property/managetenantproperty/Mzc3MzI=", false, $context);
+
+    if ($data === FALSE) {
+        echo "Error fetching the URL.";
+    } else {
+        echo $data;
+    }
+}
+
+?>
