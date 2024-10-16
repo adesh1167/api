6b6b53c8f49599ff443ca2b1fe45051bec579447
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2024-10-16 18:49:29 +0100

    Update check-access-code.php

--- public/aviator/check-access-code.php
+++ public/aviator/check-access-code.php
@@ -26,7 +26,7 @@ $access_codes = [
 ];
 
 if(isset($access_code) && !empty($access_code)){
-    if(isset($access_codes[strtolower($access_code)]){
+    if(isset($access_codes[strtolower($access_code)])){
         $response['status'] = 'valid';
         $response['data'] = $access_codes[$access_code];
         $response['message'] = 'Access Code Valid. Server overload, try again';
